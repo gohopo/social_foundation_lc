09b2c40110c9326c68fa427c4133e681b4cd616a
gohopo <gohopo@live.com>
2024-11-01 17:00:03 +0800

    chore:适配3.24.4

--- lib/social_foundation_lc_method_channel.dart
+++ lib/social_foundation_lc_method_channel.dart
@@ -3,9 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'social_foundation_lc_platform_interface.dart';
 
-/// An implementation of [SocialFoundationLcPlatform] that uses method channels.
 class MethodChannelSocialFoundationLc extends SocialFoundationLcPlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('social_foundation_lc');
 
